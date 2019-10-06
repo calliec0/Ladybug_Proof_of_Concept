f531e108234862e90002a8aa39a0c3251fcb8a83
Callie <calliec0wells@gmail.com>
2019-10-05 22:20:22 -0500

    Not Much really happened????
    -Added the ability to punch!
    -LadyBean can punch with 'E'
    -For some reason after adding the punch the physics can now somehow cause the characters to rotate diagonally, so they sometimes get stuck
    -Added a pattern to the floor to increase visibility of moving
    -Added some code to detect when being hit by something that damages you(even though damage doesn't exist yet)

--- MLB/Assets/Scripts/PlayerMovement.cs
+++ MLB/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    //Local Variables
+    public GameObject arm;
     public Rigidbody rb;
     public float verticalForce;
     public float horizontalForce;
@@ -13,13 +15,26 @@ public class PlayerMovement : MonoBehaviour
     public float turnSpeed;
     private int jumps;
 
-    void Start() { }
+    void Start() { Debug.Log(rb.gameObject.GetInstanceID()); }
 
     // Start is called before the first frame update
+   
+
+    // If we are hit by the other player, apply force from the hit (Not really sure if this is even necessary, but lets find out! 
+    void OnCollisionEnter(UnityEngine.Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.CompareTag("Damaging"))
+        {
+            rb.AddForce(collisionInfo.relativeVelocity);
+        }
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        rb.ResetCenterOfMass();
+        
         // adding vertical force 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -29,11 +44,13 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             rb.AddRelativeForce(horizontalForce * Time.deltaTime, 0, 0);
+            rb.AddTorque(0, turnSpeed, 0);
         }
 
         if (Input.GetKey(KeyCode.A)) 
         {
             rb.AddRelativeForce(-horizontalForce * Time.deltaTime, 0, 0);
+            rb.AddTorque(0, -turnSpeed, 0);
         }
 
         if (Input.GetKey(KeyCode.W))
@@ -46,14 +63,10 @@ public class PlayerMovement : MonoBehaviour
             rb.AddRelativeForce(0, 0, -backwardsForce * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.Q))
-        {
-            rb.AddTorque(0, -turnSpeed, 0);
-        }
-
         if (Input.GetKey(KeyCode.E))
         {
-            rb.AddTorque(0, turnSpeed, 0);
+            arm.GetComponent<Animator>().SetTrigger("punch");
         }
+
     }
 }
