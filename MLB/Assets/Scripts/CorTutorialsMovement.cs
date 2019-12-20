2fa41109f3821d2cff159c63b92c661236e62c2a
Callie Wells <calliec0wells@gmail.com>
2019-12-20 14:48:13 -0600

    Fixing Animations, remapping controls

---
+++ MLB/Assets/Scripts/CorTutorialsMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorTutorialsMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed = 5f;
+    float rotationSpeed = 100f;
+    Animator anim;
+
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (translation != 0)
+        {
+            anim.SetBool("Running", true);
+            anim.SetFloat("CharacterSpeed", translation);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+            anim.SetFloat("CharacterSpeed", 0); 
+        }
+    }
+}
--- MLB/Assets/Scripts/PlayerCamera.cs
+++ MLB/Assets/Scripts/PlayerCamera.cs
@@ -7,6 +7,7 @@ public class PlayerCamera : MonoBehaviour
 
     public Transform player;
     public Vector3 offset;
+    float rotationSpeed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,11 @@ public class PlayerCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.position + offset; 
+        transform.position = player.position + offset;
+        float rotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        Quaternion camTurnAngle = Quaternion.AngleAxis(rotation, Vector3.up);
+
+        offset = camTurnAngle * offset;
+        transform.LookAt(player.transform);
     }
 }
